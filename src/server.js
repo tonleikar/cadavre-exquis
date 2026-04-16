1cfdd0302a63090811cbbe7815b9187d8ab57d8e
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-04-16 16:53:27 +0100

    add room route

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const app = express();
 const server = createServer(app);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const publicDir = path.join(__dirname, '..', 'public');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+app.get('/room', (req, res) => {
+  res.sendFile(path.join(publicDir, 'room.html'));
 });
 
 server.listen(3000, () => {
